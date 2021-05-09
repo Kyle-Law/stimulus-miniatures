4d8b9b86ec5798d8482873cb434adf3b0434605a
Kyle-Law <kylelaw1121@gmail.com>
2021-05-09 15:19:13 +0800

    General-Add: Added Accordian feature

---
+++ src/controllers/accordian_controller.js
@@ -0,0 +1,18 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets=["icon","details"]
+
+  connect() {
+    console.log("Accordian controller is connected!")
+  }
+
+  toggle(){
+    this.iconTarget.classList.toggle('rotate-180')
+    if (this.detailsTarget.style.maxHeight) {
+        this.detailsTarget.style.maxHeight = null;
+      } else {
+        this.detailsTarget.style.maxHeight = this.detailsTarget.scrollHeight + "px";
+      }
+  }
+}
