6c921706257d248fa301d4a4acb95058b137dc86
Kyle-Law <kylelaw1121@gmail.com>
2021-05-14 19:17:13 +0800

    Slideshow-Edit: added interval for auto slideshow

--- src/controllers/slideshow_controller.js
+++ src/controllers/slideshow_controller.js
@@ -3,40 +3,39 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static values = {index:Number}
   static targets = ["dot","slide"]
+  static interval
 
   connect() {
     console.log("Slideshow controller is connected")
+    this.setInterval()
   }
 
   setIndex(e){
     const index = e.currentTarget.dataset.id
-    console.log(index)
     this.indexValue = index
   }
 
   next(){
-    console.log('next')
     this.indexValue = this.indexValue == 3 ? 1 : this.indexValue+1
   }
 
   prev(){
-    console.log('prev')
     this.indexValue = this.indexValue == 1 ? 3 : this.indexValue-1
   }
 
   indexValueChanged(){
-    console.log(this.indexValue)
-    console.log(this.slideTargets)
-    console.log(this.dotTargets)
+    this.setInterval()
     for (let i = 0;i < this.slideTargets.length;i++){
-      this.dotTargets[i].className.replace("active","")
+      this.dotTargets[i].classList.remove('active')
       this.slideTargets[i].style.display = "none"
     }
-    // this.dotTargets.forEach(dot => dot.className.replace("active",""))
-    // this.slideTargets.forEach(slide => slide.style.display = "none")
-
     this.slideTargets[this.indexValue - 1].style.display = "block"
-    this.dotTargets[this.indexValue - 1].className += " active"
+    this.dotTargets[this.indexValue - 1].classList.add("active")
+  }
+
+  setInterval(){
+    clearInterval(this.interval)
+    this.interval = setInterval(()=>{this.next()},3000)
   }
 
 }
