a5ec619d43b26ea7e12804fe58810e4c8eaff346
Kyle-Law <kylelaw1121@gmail.com>
2021-12-19 21:53:29 +0800

    F7-ProgressBar-Edit: Refined StimulusJS Code

--- src/controllers/progress_bar_controller.js
+++ src/controllers/progress_bar_controller.js
@@ -3,25 +3,24 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets=["innerBar","progress"]
   static values = {currentProgress:Number}
-  static progressInterval
+  static timerInterval
 
   connect() {
     console.log("Progress Bar controller is connected")
   }
 
   start(){
-    this.currentProgressValue = 1;
-    clearInterval(this.progressInterval)
-    this.progressInterval = setInterval(()=> {
+    this.reset()
+    this.timerInterval = setInterval(()=> {
       // If clicked again when the progress is running
       console.log('progressing')
-      this.currentProgressValue >= 100 ? clearInterval(this.progressInterval) : this.currentProgressValue++;
+      this.currentProgressValue >= 100 ? clearInterval(this.timerInterval) : this.currentProgressValue++;
     }, 10);
   }
 
   reset(){
     this.currentProgressValue = 0;
-    clearInterval(this.progressInterval)
+    clearInterval(this.timerInterval)
   }
 
   currentProgressValueChanged(){
