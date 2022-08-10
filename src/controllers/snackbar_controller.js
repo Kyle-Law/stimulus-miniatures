aa7d1ce22d21c6c8810d9249308e4a8ea2752f0f
Kyle-Law <kylelaw1121@gmail.com>
2022-08-10 09:54:45 +0800

    Refactor snackbar_controller, set 2.9s timeout

--- src/controllers/snackbar_controller.js
+++ src/controllers/snackbar_controller.js
@@ -9,21 +9,16 @@ export default class extends Controller {
     console.log("Snackbar controller is connected")
   }
 
-  show(){
-    // remove previous snackbar
-    this.snackbarTarget.classList.remove('show')
-    clearTimeout(this.timeout)
-    setTimeout(()=>{
-      this.snackbarTarget.classList.add("show");
+  show() {
+    this.snackbarTarget.classList.add('show');
+    this.timeout = setTimeout(() => {
+      this.removeClass();
+    }, 2900);
+  }
 
-      // After 2.9s(when it's faded out), remove show class
-      this.timeout = setTimeout(() => {
-        console.log('timing out')
-        this.snackbarTarget.classList.remove('show')
-      }, 2900);
-    },10)
-
-
+  removeClass() {
+    this.snackbarTarget.classList.remove('show');
+    clearTimeout(this.timeout);
   }
 
 }
